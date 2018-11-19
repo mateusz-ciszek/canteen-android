0fb096f4a102ee14b53515aa22b5b3ba0418d024
milkerso <dkrawczyk53@wp.pl>
2018-11-19 20:42:07 +0100

    logowanie

--- app/src/main/java/com/example/microtemp/microblog/MenuActivity.java
+++ app/src/main/java/com/example/microtemp/microblog/MenuActivity.java
@@ -14,6 +14,5 @@ public class MenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
-
     }
 }
