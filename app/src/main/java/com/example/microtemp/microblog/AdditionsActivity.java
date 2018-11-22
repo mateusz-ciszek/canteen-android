885f7564e63ed777250fb9111dea560862098777
CrossKarol <karolekkawecki@o2.pl>
2018-11-22 06:42:07 +0100

    wyrzucanie geta z menu

---
+++ app/src/main/java/com/example/microtemp/microblog/AdditionsActivity.java
@@ -0,0 +1,13 @@
+package com.example.microtemp.microblog;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AdditionsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_additions);
+    }
+}
---
+++ app/src/main/java/com/example/microtemp/microblog/api/Request.java
@@ -0,0 +1,81 @@
+package com.example.microtemp.microblog.api;
+import android.annotation.SuppressLint;
+import android.app.Service;
+import android.os.AsyncTask;
+
+import com.example.microtemp.microblog.TypeDishActivity;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class Request {
+	private ServiceCallback callback;
+	private Exception error;
+
+	public Request(ServiceCallback callback) {
+		this.callback = callback;
+	}
+
+	@SuppressLint("StaticFieldLeak")
+	public void uruchom(String l) {
+		String location=l;
+		new AsyncTask<String, String, String>() {
+			@Override
+			protected String doInBackground(String... strings) {
+				StringBuilder result = null;
+				String whereUrl = strings[0];
+				String line;
+				URL url;
+				URLConnection conn;
+				BufferedReader in = null;
+
+				try {
+					url = new URL(whereUrl);
+					conn = url.openConnection();
+					in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+					result = new StringBuilder();
+					while ((line = in.readLine()) != null) {
+						result.append(line);
+					}
+				//	callback.serviceSucces(result.toString());
+
+				} catch (MalformedURLException ex) {
+					System.err.println(ex);
+				} catch (IOException ex) {
+					System.err.println(ex);
+				} finally {
+					if (in != null) {
+						try {
+							in.close();
+						} catch (IOException ex) {
+							System.err.println(ex);
+						}
+					}
+				}
+
+				return result != null ? result.toString() : null;
+			}
+			@Override
+			protected void onPostExecute(String s)
+			{
+				if (s == null && error != null) {
+					callback.serviceFailure(error);
+					return;
+				} try {
+
+
+				callback.serviceSucces(s);
+
+
+			}catch (Exception e)
+			{
+
+			}}
+		}.execute((location));
+	}
+}
+
