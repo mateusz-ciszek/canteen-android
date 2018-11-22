00d0b6db2d6ed73b979dda32d8d42ca2a79c4af0
milkerso <dkrawczyk53@wp.pl>
2018-11-22 01:11:48 +0100

    tworzenie geta klasy data

---
+++ app/src/main/java/com/example/microtemp/microblog/api/data/Addition.java
@@ -0,0 +1,42 @@
+package com.example.microtemp.microblog.api.data;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class Addition {
+
+    @SerializedName("_id")
+    @Expose
+    private String id;
+    @SerializedName("name")
+    @Expose
+    private String name;
+    @SerializedName("price")
+    @Expose
+    private Double price;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+}
