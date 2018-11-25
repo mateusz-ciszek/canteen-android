c43765c24d7da65b9f1784cb946253fd21e2c78e
Mateusz Ciszek <202132@edu.p.lodz.pl>
2018-11-25 18:18:36 +0100

    Token jest zapisywany w SharedPreferences

--- app/src/main/java/com/example/microtemp/microblog/LoginActivity.java
+++ app/src/main/java/com/example/microtemp/microblog/LoginActivity.java
@@ -1,7 +1,9 @@
 package com.example.microtemp.microblog;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -84,8 +86,14 @@ public class LoginActivity extends AppCompatActivity {
         @Override
         protected void onPostExecute(LoginResponse result) {
             if (result.getHttpStatusCode() == 200) {
+                final String token = result.getData().getToken();
 
-                JWT jwt = new JWT(result.getData().getToken());
+                SharedPreferences.Editor editor
+                        = PreferenceManager.getDefaultSharedPreferences(App.getContext()).edit();
+                editor.putString("token", token);
+                editor.apply();
+
+                JWT jwt = new JWT(token);
                 boolean isAdmin = jwt.getClaim("admin").asBoolean();
 
                 Intent intent;
