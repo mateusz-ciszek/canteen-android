0b9f311b16142e9ca48063956d3764960562669c
milkerso <dkrawczyk53@wp.pl>
2018-12-05 19:16:17 +0100

    Dodawanie Menu

--- app/src/main/java/com/example/microtemp/microblog/activity/administration/menu/list/AddMenuActivity.java
+++ app/src/main/java/com/example/microtemp/microblog/activity/administration/menu/list/AddMenuActivity.java
@@ -5,26 +5,66 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.example.microtemp.microblog.App;
 import com.example.microtemp.microblog.R;
+import com.example.microtemp.microblog.activity.administration.dashboard.AdminDashboardActivity;
+import com.example.microtemp.microblog.api.HttpRequestData;
+import com.example.microtemp.microblog.api.HttpRequestMethods;
+import com.example.microtemp.microblog.api.handlers.AddMenuRequestHandler;
+import com.example.microtemp.microblog.api.models.requests.AddMenuRequestBody;
+import com.example.microtemp.microblog.api.models.responses.AddMenuResponse;
 
 public class AddMenuActivity extends AppCompatActivity {
-
-    Button acceptButton;
+    private Button acceptBtn;
+    private EditText nameEt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R .layout.activity_add_menu);
-        acceptButton = findViewById(R.id.acceptButton);
+        setContentView(R.layout.activity_add_menu);
+        acceptBtn = findViewById(R.id.acceptButton);
+        nameEt = findViewById(R.id.et_addMenu);
 
-        acceptButton.setOnClickListener(new View.OnClickListener() {
+        acceptBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(AddMenuActivity.this,
-                        MenusListManagementActivity.class);
-                startActivity(intent);
-            }
-        });
+                if (nameEt.getText().toString().length() != 0) {
+                    AddMenuRequestBody requestBody = AddMenuRequestBody.builder()
+                            .name(nameEt.getText().toString())
+                            .build();
 
+                    HttpRequestData<AddMenuRequestBody> requestData = HttpRequestData.<AddMenuRequestBody>builder()
+                            .requestBody(requestBody)
+                            .method(HttpRequestMethods.POST)
+                            .build();
+                    new AddMenuRequestHandler() {
+                        @Override
+                        protected void onPostExecute(AddMenuResponse result) {
+                            if (result.getHttpStatusCode() == 400) {
+                                Toast.makeText(acceptBtn.getContext(),
+                                        "Cant add menu",
+                                        Toast.LENGTH_LONG).show();
+                            } else if (result.getHttpStatusCode() == 201) {
+
+                                Toast.makeText(acceptBtn.getContext(),
+                                        "Dish added",
+                                        Toast.LENGTH_LONG).show();
+                                Intent intent = new Intent(App.getContext(), AdminDashboardActivity.class);
+                                App.getContext().startActivity(intent);
+
+                            } else {
+                                Intent intent = new Intent(AddMenuActivity.this, AdminDashboardActivity.class);
+                                intent.putExtra("response", result);
+                                startActivity(intent);
+                            }
+                        }
+                    }.execute(requestData);
+                }
+            }
+
+
+        });
     }
 }
