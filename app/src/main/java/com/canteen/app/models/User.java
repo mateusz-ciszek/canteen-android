0e5a8efaedba8628f8dd55e14f3ae76e86287a2d
Mateusz Ciszek <202132@edu.p.lodz.pl>
2019-01-14 19:57:12 +0100

    Fix - User implements Serializable

--- app/src/main/java/com/canteen/app/models/User.java
+++ app/src/main/java/com/canteen/app/models/User.java
@@ -1,9 +1,11 @@
 package com.canteen.app.models;
 
+import java.io.Serializable;
+
 import lombok.Getter;
 
 @Getter
-public class User {
+public class User implements Serializable {
     private String _id;
     private String email;
     private String firstName;
