802db4e48663d1a238462da837c4e81feffecc44
Mateusz Ciszek <202132@edu.p.lodz.pl>
2019-08-24 01:54:54 +0200

    Refactored order cart activity
    - using ButterKnife injections
    - unified naming

--- app/src/main/java/com/canteen/app/activity/cart/OrderItemsListViewHolder.java
+++ app/src/main/java/com/canteen/app/activity/cart/OrderItemsListViewHolder.java
@@ -2,50 +2,54 @@ package com.canteen.app.activity.cart;
 
 import android.support.constraint.ConstraintLayout;
 import android.support.v7.widget.RecyclerView;
-import android.widget.ImageButton;
 import android.widget.TextView;
 
 import com.canteen.app.R;
-import com.canteen.app.models.FoodAddition;
 import com.canteen.app.service.order.OrderCartService;
 import com.canteen.app.service.order.OrderItem;
 
 import java.util.Locale;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 class OrderItemsListViewHolder extends RecyclerView.ViewHolder {
+
     private OrderItem orderItem;
-    private TextView foodNameTextView;
-    private TextView foodPriceTextView;
-    private TextView selectedAdditionsTextView;
-    private ImageButton removeItemButton;
+
+    @BindView(R.id.food_name_text_view)
+    TextView foodNameTextView;
+
+    @BindView(R.id.food_price_text_view)
+    TextView foodPriceTextView;
+
+    @BindView(R.id.selected_additions_text_view)
+    TextView selectedAdditionsTextView;
 
     OrderItemsListViewHolder(ConstraintLayout itemView) {
         super(itemView);
+        ButterKnife.bind(this, itemView);
+    }
 
-        this.foodNameTextView = itemView.findViewById(R.id.foodNameTextView);
-        this.foodPriceTextView = itemView.findViewById(R.id.foodPriceTextView);
-        this.selectedAdditionsTextView = itemView.findViewById(R.id.selectedAdditionsTextView);
-        this.removeItemButton = itemView.findViewById(R.id.removeItemButton);
+    @OnClick(R.id.removeItemButton)
+    void removeItemButtonHandler() {
+        OrderCartService.getInstance().removeItem(orderItem);
     }
 
     void setOrderItem(final OrderItem orderItem) {
         this.orderItem = orderItem;
-
-        this.foodNameTextView.setText(this.orderItem.getFood().getName());
-        this.foodPriceTextView.setText(String.format(Locale.getDefault(), "%.2f zł", this.orderItem.getPrice()));
+        foodNameTextView.setText(orderItem.getFood().getName());
+        foodPriceTextView.setText(String.format(Locale.getDefault(), "%.2f zł", orderItem.getPrice()));
 
         StringBuilder selectedItemsText;
-        if (this.orderItem.getAdditions().size() == 0) {
+        if (orderItem.getAdditions().size() == 0) {
             selectedItemsText = new StringBuilder("No additions");
         } else {
             selectedItemsText = new StringBuilder("Selected additions: ");
-            for (FoodAddition addition : this.orderItem.getAdditions()) {
-                selectedItemsText.append(addition.getName()).append(", ");
-            }
+            orderItem.getAdditions().forEach(addition -> selectedItemsText.append(addition.getName()).append(", "));
             selectedItemsText.substring(0, selectedItemsText.length() - 2);
         }
-        this.selectedAdditionsTextView.setText(selectedItemsText.toString());
-
-        this.removeItemButton.setOnClickListener(v -> OrderCartService.getInstance().removeItem(orderItem));
+        selectedAdditionsTextView.setText(selectedItemsText.toString());
     }
 }
